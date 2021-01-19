e0fafce49fd5154cb041f66f47fdd923e3e8ecf2
Ysrael <Razyelito>
2021-01-19 12:29:00 -0500

    files

---
+++ src/test/java/com/espol/rentalsystemtest/MovieTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.espol.rentalsystemtest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Ysrael
+ */
+public class MovieTest {
+    
+    public MovieTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getPriceCode method, of class Movie.
+     */
+    @Test
+    public void testGetPriceCode() {
+        System.out.println("getPriceCode");
+        Movie instance = new Movie("Lord of the Rings",Movie.REGULAR);
+        int expResult = Movie.REGULAR;
+        int result = instance.getPriceCode();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPriceCode method, of class Movie.
+     */
+    @Test
+    public void testSetPriceCode() {
+        System.out.println("setPriceCode");
+        int arg = 0;
+        Movie instance = null;
+        instance.setPriceCode(arg);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
